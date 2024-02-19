0acb50d8fd7488eaec4054619ae809e7bca4ca2f
Chris Olcheski <24864308+colcheski@users.noreply.github.com>
2024-02-19 18:48:25 -0500

    add modal service and ability to register and open individual modals by ids

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.css'
+  styleUrl: './modal.component.css',
+  //providers: [ModalService]
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+  @Input() modalID = '';
 
+  constructor(public modal: ModalService) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID);
+  }
 }
