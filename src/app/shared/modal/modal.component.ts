c777f10f675cc13f58aea5698069e1a1171ace56
Chris Olcheski <24864308+colcheski@users.noreply.github.com>
2024-02-19 19:20:32 -0500

    always move the modal component to the body so that it doesn't inherit parent css

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 
 @Component({
@@ -10,12 +10,15 @@ import { ModalService } from '../../services/modal.service';
 export class ModalComponent implements OnInit {
   @Input() modalID = '';
 
-  constructor(public modal: ModalService) {
+  constructor(
+    public modal: ModalService,
+    public el: ElementRef
+    ) {
 
   }
 
   ngOnInit(): void {
-
+    document.body.appendChild(this.el.nativeElement);
   }
 
   closeModal() {
